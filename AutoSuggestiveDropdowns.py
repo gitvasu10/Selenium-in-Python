fe358f26f7d9f016fd8e458bfdb847b6235d9f5c
Shashwat Malasi <developervasu10@gmail.com>
2024-05-06 11:01:24 +0530

    Commit upto dropdowns

--- AutoSuggestiveDropdowns.py
+++ AutoSuggestiveDropdowns.py
@@ -11,12 +11,13 @@ countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")
 print("The number of countries in this dropdown menu are: {}".format(len(countries)))
 
 for country in countries:
-  # if(country.text == "India"):
-   assert country.text == "India", "Country could not be found!"
-   country.click()
-   break
+   if(country.text == "India"):
+  # assert country.text == "India", "Country could not be found!"
+        country.click()
+        break
 
-
-
+#print(driver.find_element(By.ID, "autosuggest").text) # This only helps in case of the pre-filled text data, not for the dynamically filled text
+print(driver.find_element(By.ID, "autosuggest").get_attribute("value")) #This method helps to catch the dynamically filled data
+assert driver.find_element(By.ID, "autosuggest").get_attribute("value") == "India"
 
 time.sleep(5)
