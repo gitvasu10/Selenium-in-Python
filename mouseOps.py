4bc9bb1bfc6ef0902dc37fa8f0a213d25b7c6c58
Shashwat Malasi <developervasu10@gmail.com>
2024-05-08 14:28:40 +0530

    Commit for mouse operations

---
+++ mouseOps.py
@@ -0,0 +1,24 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+action = ActionChains(driver)
+#action.click_and_hold(driver.find_element()).perform() #For long click
+#action.context_click().perform() #Right click
+#action.double_click().perform()
+
+driver.implicitly_wait(15)
+driver.maximize_window()
+action.move_to_element(driver.find_element(By.ID, "mousehover")).perform()
+time.sleep(2)
+#wait = WebDriverWait(driver, 10)
+#wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "Top")))
+action.context_click(driver.find_element(By.LINK_TEXT, "Top")).perform()
+time.sleep(2)
+#action.move_to_element(driver.find_element(By.XPATH, "//input[@value='radio2']")).click().perform()
+time.sleep(2)
