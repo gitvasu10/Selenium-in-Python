6fb037e74bdbb99d854a78ac1162433cbcfb0bd8
Shashwat Malasi <56060808+gitvasu10@users.noreply.github.com>
2024-05-09 10:10:34 +0530

    Child windows switch assignment-3

--- Assignment-3.py
+++ Assignment-3.py
@@ -1,11 +1,10 @@
-#This code does go the first website and from there goes to the second usign the hyperlink and extract a section of text out of it
+#This code does go the first website and from there goes to the second usign the hyperlink and extract a section of text out of it.
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get("https://www.rahulshettyacademy.com/loginpagePractise/") #Parent website
-
 
 driver.find_element(By.CLASS_NAME, "blinkingText").click() #Click the hyperlink
 
@@ -14,7 +13,6 @@ opened_windows = driver.window_handles
 driver.switch_to.window(opened_windows[1])
 print(driver.find_element(By.XPATH, "//a[text()='mentor@rahulshettyacademy.com']").text) #Extract the text content from this website
 
- 
 
 
 
