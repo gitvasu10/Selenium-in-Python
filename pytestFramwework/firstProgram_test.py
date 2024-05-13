800e79166b506ec23d55b2d190a1b89b96d0d1a3
Shashwat Malasi <developervasu10@gmail.com>
2024-05-13 13:01:36 +0530

    Running test cases in pytest

--- pytestFramwework/firstProgram_test.py
+++ pytestFramwework/firstProgram_test.py
@@ -1,4 +1,29 @@
-#Ant pytest file either must start with '_test' or end with 'test_'
+#1 Ant pytest file either must start with 'test' or end with 'test'
+#2 Methods' name must begin with 'test'
+#3 The entire code logic should be encapsulated in the method body
 
-def firstFunction_test():
-    print("This is a pytest test function!")
+#4 -v --> Verbose, provides extra details regarding the test cases
+#  -s --> Fof printing the entire contents from the test methods
+#  -k --> For running the specific test methods from different files
+#  -m --> For marking the test cases to run some specific ones only
+
+import pytest
+
+def test_firstFunction():
+   #print("This is a pytest test function!")
+    message = "Hello"
+    assert message == "Hi", "The strings do not match!"
+
+def test_secondFunction():
+    #print("Into the second test function!")
+    a = 4
+    b = 6
+    assert b-2 == a, "The values do not match!"
+
+
+def test_shopping_cart():
+    print("This is the shopping cart test case in 1st file")
+
+@pytest.mark.PistonCup
+def test_car():
+    print("I am speed!")
--- pytestFramwework/test_demo.py
+++ pytestFramwework/test_demo.py
@@ -1,2 +1,11 @@
+import pytest
+
 def test_demoFunction():
     print('Hello!')
+
+def test_shopping():
+    print("This is the test case for shopping cart in the second file!")
+
+@pytest.mark.PistonCup
+def test_car():
+    print("I am Lightning McQueen!")
