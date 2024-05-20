9600d281bdbcf4716c2edf4567082fc9165e2200
Shashwat Malasi <56060808+gitvasu10@users.noreply.github.com>
2024-05-20 11:33:27 +0530

    Update test_fixtureData.py

--- pytestFramwework/test_fixtureData.py
+++ pytestFramwework/test_fixtureData.py
@@ -4,7 +4,6 @@ class TestExample:
     def test_edit(self, dataLoad): #If we want to return something from the fixture, then we have to pass the parameter in the function itself
         print(dataLoad)
 #On declaring the fixture name globally, we don;t need to pass the parameter in the function declaration.
-<<<<<<< HEAD
 #Only in the case where the fixture is returning something that we need to explicitly mention the fixture name in the function parameter list
 
 
@@ -12,4 +11,3 @@ class TestExample:
 =======
 #Only in the case where the fixture is returning something that we need to explicitly mention the fixture name in the function parameter list.
 
->>>>>>> acf9099b3b11ef0f98c07fc639ecf48608824a9a
