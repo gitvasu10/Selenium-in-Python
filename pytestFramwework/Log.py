b072bb693ab57f7ce826c8e9f0123bc19aebb2c2
Shashwat Malasi <56060808+gitvasu10@users.noreply.github.com>
2024-05-20 13:45:47 +0530

    Class containing the logger function definition

--- pytestFramwework/Log.py
+++ pytestFramwework/Log.py
@@ -3,7 +3,7 @@ import logging
 class Log:
     def log_function(self):
         loggerName = inspect.stack()[1][3] #This scans the stack trace to know which class has called the function
-        logger = logging.getLogger(loggerName)
+        logger = logging.getLogger(loggerName) #That function name will be passed into the logger
         file_handler = logging.FileHandler("logfile.log")
         formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s : %(message)s")
         file_handler.setFormatter(formatter)
