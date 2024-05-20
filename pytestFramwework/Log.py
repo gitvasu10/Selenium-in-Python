e596ae5a594131b9c0d4f443906c72d033554d1d
Shashwat Malasi <developervasu10@gmail.com>
2024-05-20 13:41:27 +0530

    Using log function in different classes

---
+++ pytestFramwework/Log.py
@@ -0,0 +1,12 @@
+import inspect
+import logging
+class Log:
+    def log_function(self):
+        loggerName = inspect.stack()[1][3] #This scans the stack trace to know which class has called the function
+        logger = logging.getLogger(loggerName)
+        file_handler = logging.FileHandler("logfile.log")
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s : %(message)s")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        logger.setLevel(logging.DEBUG)
+        return logger
--- pytestFramwework/test_fixtureData.py
+++ pytestFramwework/test_fixtureData.py
@@ -1,15 +1,20 @@
 import pytest
+
+from pytestFramwework.Log import Log
+
+
 @pytest.mark.usefixtures("dataLoad") #Annotation for including fixtures from the conftest file into the current one
-class TestExample:
+class TestExample(Log):
     def test_edit(self, dataLoad): #If we want to return something from the fixture, then we have to pass the parameter in the function itself
-        print(dataLoad)
+        log = self.log_function()
+        log.info(dataLoad[0])
+        log.info(dataLoad[1])
 #On declaring the fixture name globally, we don;t need to pass the parameter in the function declaration.
-<<<<<<< HEAD
+
 #Only in the case where the fixture is returning something that we need to explicitly mention the fixture name in the function parameter list
 
 
 #--------14th May ------------------Running the data with multiple set of test runs
-=======
 #Only in the case where the fixture is returning something that we need to explicitly mention the fixture name in the function parameter list.
 
->>>>>>> acf9099b3b11ef0f98c07fc639ecf48608824a9a
+
