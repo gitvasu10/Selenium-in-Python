4362f4cc153d44b4ed08e4e4e04cb35d11f3b6d7
Shashwat Malasi <developervasu10@gmail.com>
2024-06-05 22:22:38 +0530

    Code ready to push

--- pytestFramwework/utilities/BaseClass.py
+++ pytestFramwework/utilities/BaseClass.py
@@ -1,7 +1,10 @@
 import pytest
+from selenium import webdriver
 
 
 @pytest.mark.usefixtures(
     "setup")  #In the conftest, we create a fixture and in this base class, we are using the fixture, thereafter this class is being inherited by the e2e_copy file
 class BaseClass:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
     pass
